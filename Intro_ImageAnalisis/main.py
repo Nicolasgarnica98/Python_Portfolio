d7bad6d6b8758fcd5d0d301af0f1a1608cf6414a
Nicolas G <nickgc98@hotmail.com>
2022-07-21 12:22:51 -0400

    Intro to image analisis

--- Intro_ImageAnalisis/main.py
+++ Intro_ImageAnalisis/main.py
@@ -5,30 +5,29 @@ from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 import os
 
-#Descarga de la imagen seleccionada
-r = requests.get('https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/cc_Giraffes_16x9.jpg?itok=dKmuVKO6')
-with open('img_jirafas.png', 'wb') as f:
+#Image download
+r = requests.get('https://animalesmascotas.com//wp-content/uploads/2016/08/guepardos-familia-de-guepardos.jpg')
+with open('Intro_ImageAnalisis/img_cheeta.jpg', 'wb') as f:
       f.write(r.content)
 
-#Carga de la imágen seleccionada
-img_jirafas = io.imread(os.path.join(('img_jirafas.png')))
-print(img_jirafas.shape)
-#Obtencion de canales de la imagen
+#Image load
+img_cheeta = io.imread(os.path.join(('Intro_ImageAnalisis/img_cheeta.jpg')))
+print(img_cheeta.shape)
 
-#Canal R
-r_channel = img_jirafas.copy()
+#Red channel
+r_channel = img_cheeta.copy()
 r_channel[:,:,1] = 0
 r_channel[:,:,2] = 0
 r_channel = rgb2gray(r_channel)
 
-#Canal G
-g_channel = img_jirafas.copy()
+#Green channel
+g_channel = img_cheeta.copy()
 g_channel[:,:,0] = 0
 g_channel[:,:,2] = 0
 g_channel = rgb2gray(g_channel)
 
-#Canal B
-b_channel = img_jirafas.copy()
+#Blue channel
+b_channel = img_cheeta.copy()
 b_channel[:,:,0] = 0
 b_channel[:,:,1] = 0
 b_channel = rgb2gray(b_channel)
@@ -36,45 +35,46 @@ b_channel = rgb2gray(b_channel)
 
 #Subplot 1
 fig, ax = plt.subplots(2,2)
-fig.suptitle('Canales de la imagen RGB')
-ax[0][0].imshow(img_jirafas)
+fig.suptitle('Image channel separation')
+ax[0][0].imshow(img_cheeta)
 ax[0][0].axis('off')
-ax[0][0].set_title('Imagen a color (RGB)')
+ax[0][0].set_title('RGB')
 ax[0][1].imshow(r_channel, cmap='gray')
 ax[0][1].axis('off')
-ax[0][1].set_title('Primer canal (R)')
+ax[0][1].set_title('Red')
 ax[1][0].imshow(g_channel, cmap='gray')
 ax[1][0].axis('off')
-ax[1][0].set_title('Segundo canal (G)')
+ax[1][0].set_title('Green')
 ax[1][1].imshow(b_channel, cmap='gray')
 ax[1][1].axis('off')
-ax[1][1].set_title('Tercer canal (B)')
+ax[1][1].set_title('Blue')
 fig.tight_layout()
-plt.savefig('Figura 1')
+plt.savefig('Intro_ImageAnalisis/Figure1')
 plt.show()
 
-#Conversion de la imagen original a grises
-grayimg_jirafas = rgb2gray(img_jirafas)
+#Gray scaled image
+grayimg_cheeta = rgb2gray(img_cheeta)
 # plt.plot(grayimg_jirafas)
 # plt.show()
-#Obtencion de los histogramas
-hist_color = img_jirafas.flatten()
-hist_gris = grayimg_jirafas.flatten()
+
+#Image histogram
+hist_color = img_cheeta.flatten()
+hist_gris = grayimg_cheeta.flatten()
 
 #Subplot 2
 input('Press enter to continue... ')
 fig1, ax1 = plt.subplots(2,2,)
-ax1[0][0].imshow(img_jirafas)
+ax1[0][0].imshow(img_cheeta)
 ax1[0][0].axis('off')
-ax1[0][0].set_title('Imagen a color (RGB)')
+ax1[0][0].set_title('RGB')
 ax1[0][1].hist(hist_color,bins=255)
-ax1[0][1].set_title('Histograma imagen a color')
-ax1[1][0].imshow(grayimg_jirafas, cmap='gray')
+ax1[0][1].set_title('RGB image histogram')
+ax1[1][0].imshow(grayimg_cheeta, cmap='gray')
 ax1[1][0].axis('off')
-ax1[1][0].set_title('Imagen en grises')
+ax1[1][0].set_title('Gray-scale image')
 ax1[1][1].hist(hist_gris,bins=255)
-ax1[1][1].set_title('Histograma imagen en grises')
+ax1[1][1].set_title('Histogram')
 fig1.tight_layout()
 
-plt.savefig('Figura 2')
+plt.savefig('Intro_ImageAnalisis/Figure2')
 plt.show()
